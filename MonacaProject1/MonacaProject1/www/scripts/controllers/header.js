e0d478b1de546385026be215d010f96f1a39dd29
mochan-tk <takashi1.kawamoto@gmail.com>
2015-08-05 18:22:10 +0900

    　

--- MonacaProject1/MonacaProject1/www/scripts/controllers/header.js
+++ MonacaProject1/MonacaProject1/www/scripts/controllers/header.js
@@ -20,5 +20,25 @@ angular.module('myApp')
       ];
       vm.addList = function (name) {
           return { name: name };
-      }
+      };
+
+      $scope.items = [
+  'The first choice!',
+  'And another choice for you.',
+  'but wait! A third!'
+      ];
+
+      $scope.status = {
+          isopen: false
+      };
+
+      $scope.toggled = function (open) {
+          $log.log('Dropdown is now: ', open);
+      };
+
+      $scope.toggleDropdown = function ($event) {
+          $event.preventDefault();
+          $event.stopPropagation();
+          $scope.status.isopen = !$scope.status.isopen;
+      };
   });
